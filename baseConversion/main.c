45b76eec524f95c93fc270d8558b2b74309cedc8
Andrew Chin <andrew_chin@tamu.edu>
2019-11-28 17:42:38 -0600

    add practice feature

--- baseConversion/main.c
+++ baseConversion/main.c
@@ -3,9 +3,7 @@
 //    Andrew Chin    Nov 28, 2019
 //=======================================
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "num_conversion.h"
 
 #define BUF_SIZE 100
@@ -14,6 +12,7 @@ int main(void) {
    int out_base = 10; // 2, 8, 10, 16
    char input[BUF_SIZE];
    char result[BUF_SIZE];
+   srand(time(0));
 
    // TODO: implement history feature for previous user input
 
@@ -55,8 +54,82 @@ int main(void) {
          }
          
       } else if (strncmp(input, "practice", 8) == 0) {
-         // TODO:implement
-         printf("TODO\n");
+         // TODO: redo this feature -- VERY ugly
+         printf("PRACTICE MODE: type \"quit\" to return to shell\n");
+         do {
+            int start = 2;//rand() % 4; // [0,3]
+            int end = rand() % 4; // [0,3]
+            int r = rand() % 16 + 1; // [1, 16]
+            char buf[BUF_SIZE];
+            printf("[p]>>> ");
+            switch (start) {
+               case 0:
+                  sprintf(buf, "%d", r);
+                  printf("convert 0b%ld to base ", strtol(buf, NULL, 2));
+                  break;
+               case 1:
+                  printf("convert 0%o to base ", r);
+                  break;
+               case 2:
+                  printf("convert %d to base ", r);
+                  break;
+               case 3:
+                  printf("convert 0x%x to base ", r);
+                  break;
+               default:
+                  break;
+            }
+            switch (end) {
+               case 0:
+                  printf("2: ");
+                  break;
+               case 1:
+                  printf("8: ");
+                  break;
+               case 2:
+                  printf("10: ");
+                  break;
+               case 3:
+                  printf("16: ");
+                  break;
+               default:
+                  break;
+            }
+            fgets(input, sizeof(input), stdin);
+            input[strlen(input)-1] = '\0'; // replace '\n'
+            char ans[BUF_SIZE];
+            if (end == 0) {
+               convert(input, buf, 2); // TODO: we do not check if convert fails
+               char temp[BUF_SIZE];
+               sprintf(temp, "%d", r);
+               convert(temp, ans, 2);
+            } else if (end == 1) {
+               convert(input, buf, 8);
+               char temp[BUF_SIZE];
+               sprintf(temp, "%d", r);
+               convert(temp, ans, 8);
+            } else if (end == 2) {
+               convert(input, buf, 10);
+               char temp[BUF_SIZE];
+               sprintf(temp, "%d", r);
+               convert(temp, ans, 10);
+            } else if (end == 3) {
+               convert(input, buf, 16);
+               char temp[BUF_SIZE];
+               sprintf(temp, "%d", r);
+               convert(temp, ans, 16);
+            }
+            if (strncmp(input, "quit", 4) == 0) {
+               input[0] = '\0';
+               break;
+            }
+            if (input[0] != '\0' && strncmp(input, buf, strlen(input)) == 0) {
+               printf("       correct!\n");
+            } else {
+               printf("       wrong! Correct answer is %s\n", ans);
+            }
+
+         } while (strncmp(input, "quit", 4) != 0);
 
       } else if (strncmp(input, "quit", 4) == 0) {
          return 0;
